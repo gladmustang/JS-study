3dbb80877838c7d941879bc82a53e7ef98e185b4
Jason Shen <jshen103@nam.ad.pwcinternal.com>
2017-09-09 11:19:53 +0800

    add router

--- react-redux-template/src/js/App.js
+++ react-redux-template/src/js/App.js
@@ -2,13 +2,14 @@
  * Created by jshen103 on 9/6/2017.
  */
 import React from 'react'
-import { connect } from 'react-redux'
 import {CounterContainer} from './components/Counter';
+import BasicRouterExample from './components/Router1'
 
 const App = (props) => {
     return  (
         <div>
             <CounterContainer />
+            <BasicRouterExample/>
         </div>
 
     )
