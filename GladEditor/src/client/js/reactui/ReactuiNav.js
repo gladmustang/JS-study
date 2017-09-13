5182a416ed378910fc84656f343a2211ae1ac168
Jason Shen <jshen103@nam.ad.pwcinternal.com>
2017-09-13 22:26:44 +0800

    update

--- GladEditor/src/client/js/reactui/ReactuiNav.js
+++ GladEditor/src/client/js/reactui/ReactuiNav.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MainTab from './pages/MainTab'
 
-const ReactuiNav = () => (
-    <MuiThemeProvider>
-        <div>
-            <AppBar style={{
-                marginBottom: '1px'
-            }}
-                title="Glad Editor"
-                iconClassNameRight="fa fa-heart-o"
-            />
-            <MainTab/>
-        </div>
-    </MuiThemeProvider>
-);
+class ReactuiNav extends Component {
+    rightButtonClick = ()=> {
+        alert("hello")
+    }
+    render(){
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <AppBar style={{
+                        marginBottom: '1px'
+                    }}
+                            title="Glad Editor"
+                            iconClassNameRight="fa fa-times"
+                            onRightIconButtonTouchTap={this.rightButtonClick}
+                    />
+                    <MainTab/>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
+
 export default ReactuiNav;
